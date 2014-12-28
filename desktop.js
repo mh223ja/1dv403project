5f3a8b183dfd32ccad1796a09b0ce38adbadf8e7
mh223ja <mh223ja@student.lnu.se>
2014-12-28 22:19:49 +0000

    progress

--- desktop.js
+++ desktop.js
@@ -2,36 +2,63 @@
 
 "use strict";
 var Project = {
-    
-    init: function(){
+
+    thumbnails: null,
+    background: null,
+
+    init: function() {
+        console.log("hi");
         Project.renderDesktop();
         Project.renderWindow();
         Project.getImages();
-},
-    RenderDesktop: function(){
+        Project.changeImage();
+    },
+
+    RenderDesktop: function() {
+
+        Project.background = document.body.style.backgroundImage = "url('pics/desktop.jpg')";
+
         var footer = document.getElementById("footer");
         var iconDiv = document.createElement("div");
         footer.appendChild("iconDiv");
-        
+
         var iconImage = document.createElement("img");
-        iconImage.setAttribute("src","link");
+        iconImage.setAttribute("src", "pics/icon.jpg");
         iconDiv.appendChild("iconImage");
-        
+
+
+        console.log("hi");
     },
-    
-    RenderWindow: function(){
-    var containerDiv = document.getElementById("container");
-    var windowDiv = document.createElement("div");
-    containerDiv.appendChild("windowDiv");
-    
-    var windowHeader= document.createElement("div");
+
+    RenderWindow: function() {
+        var containerDiv = document.getElementById("container");
+        var windowDiv = document.createElement("div");
+        containerDiv.appendChild("windowDiv");
+
+        var windowHeader = document.createElement("div");
         windowDiv.appendChild("windowHeader");
-     
-    var headerText = document.createElement("p");
+
+        var headerText = document.createElement("p");
         windowHeader.appendChild("headerText");
-        headerText.innerHTML = "Image Gallery";
-    
-    
-}
+        headerText.innerHTML = "Image Viewer";
+        console.log("there");
+
+
+    },
+
+    getImages: function() {
+        var thumbnailInfo;
+        var xhr = new XMLHttpRequest();
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                thumbnailInfo = JSON.parse(xhr.responseText);
+            }
+        }
+    },
+
+    changeImage: function() {
+
+    }
+
 };
-    window.onload = Project.init;
+window.onload = Project.init;
