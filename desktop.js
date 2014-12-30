e68217f1776c77ab3ae0dbac419f5e8b9ab4c853
mh223ja <mh223ja@student.lnu.se>
2014-12-30 23:15:49 +0000

    moving along

--- desktop.js
+++ desktop.js
@@ -9,41 +9,55 @@ var Project = {
     init: function() {
         console.log("hi");
         Project.renderDesktop();
-        Project.renderWindow();
-        Project.getImages();
-        Project.changeImage();
+        //Project.renderWindow();
+        //Project.getImages();
+        //Project.changeImage();
     },
 
-    RenderDesktop: function() {
-
-        Project.background = document.body.style.backgroundImage = "url('pics/desktop.jpg')";
+    renderDesktop: function() {
+        
 
         var footer = document.getElementById("footer");
         var iconDiv = document.createElement("div");
-        footer.appendChild("iconDiv");
+        footer.appendChild(iconDiv);
 
         var iconImage = document.createElement("img");
         iconImage.setAttribute("src", "pics/icon.jpg");
-        iconDiv.appendChild("iconImage");
+        iconImage.className = "iconSettings";
+        //iconDiv.appendChild(iconImage);
+        
+        var iconInfo = document.createElement("a");
+        iconInfo.setAttribute("href", "#");
+        //iconImage.appendChild(imageInfo);
+        iconInfo.appendChild(iconImage);
+        iconDiv.appendChild(iconInfo);
+        
+        var desktopText = document.createElement("p");
+        desktopText.className = "textSettings";
+        footer.appendChild(desktopText);
+        desktopText.innerHTML = "Morgan's fab desktop gallery";
+        
+        iconInfo.onClick = Project.renderWindow (); 
+        
+           
+          
 
-
-        console.log("hi");
+        console.log("hi again");
     },
 
-    RenderWindow: function() {
+    renderWindow: function() {
+        console.log("here");
         var containerDiv = document.getElementById("container");
         var windowDiv = document.createElement("div");
-        containerDiv.appendChild("windowDiv");
+        containerDiv.appendChild(windowDiv);
 
         var windowHeader = document.createElement("div");
-        windowDiv.appendChild("windowHeader");
+        windowDiv.appendChild(windowHeader);
 
         var headerText = document.createElement("p");
-        windowHeader.appendChild("headerText");
+        windowHeader.appendChild(headerText);
         headerText.innerHTML = "Image Viewer";
         console.log("there");
-
-
     },
 
     getImages: function() {
